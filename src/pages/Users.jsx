0ee8ea632764b947f8683e769f4875ea64761651
Jason <jasondoroughonfb@live.com>
2025-06-07 23:44:11 -0700

    Twelfth commit

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,11 +3,11 @@ import { useParams } from "react-router";
 import React, {useEffect} from "react";
 
 function Users() {
-    const {username} = useParams(); //const name must match dynamic route name here
+    const {id} = useParams(); //const name must match dynamic route name here
     useEffect(() => {
-        // fetch(`https://api.instagram.com/${username}`) just a guess as an example
+        // fetch(`https://api.instagram.com/${username}`) just an example
     }, [])
-    return <h1>{username}</h1>; // same const name here, too
+    return <h1>{id}</h1>; // same const name here, too
 }
 
 export default Users;
