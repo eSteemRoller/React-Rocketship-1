c8d852fdb6f35663d222f98cd80214ff45305995
Jason <jasondoroughonfb@live.com>
2025-06-10 22:32:25 -0700

    Fourteenth commit

--- src/pages/Home copy 2.jsx
+++ src/pages/Home copy 2.jsx
@@ -34,5 +34,67 @@ function Home() {
         </div>
     );
 }
+function Home() {
+
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(data);
+    }
+
+    useEffect(() => {
+
+        fetchUsers();
+    }, []);
+
+    const pixels = "2px";
+
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user.id} style={{border: `${pixels} solid black`}}>
+                    <div>{user?.id}</div>
+                    <div>{user?.name}</div>
+                    <div>{user?.email}</div>
+                    <div>{user?.username}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+function Home() {
+
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+        const { data } = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(data);
+    }
+
+    useEffect(() => {
+
+        fetchUsers();
+    }, []);
+
+    const pixels = "2px";
+
+    return (
+        <div>
+            {users.map((user) => (
+                <div key={user.id} style={{border: `${pixels} solid black`}}>
+                    <div>{user?.id}</div>
+                    <div>{user?.name}</div>
+                    <div>{user?.email}</div>
+                    <div>{user?.username}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
 
 export default Home;
--- src/pages/Users copy.jsx
+++ src/pages/Users copy.jsx
@@ -11,5 +11,19 @@ function Users() {
     }, [])
     return <h1>{id}</h1>; // same const name here, too
 }
+function Users() {
+    const {id} = useParams(); //const name must match dynamic route name here
+    useEffect(() => {
+        // fetch(`https://api.instagram.com/${username}`) just an example
+    }, [])
+    return <h1>{id}</h1>; // same const name here, too
+}
+function Users() {
+    const {id} = useParams(); //const name must match dynamic route name here
+    useEffect(() => {
+        // fetch(`https://api.instagram.com/${username}`) just an example
+    }, [])
+    return <h1>{id}</h1>; // same const name here, too
+}
 
 export default Users;
